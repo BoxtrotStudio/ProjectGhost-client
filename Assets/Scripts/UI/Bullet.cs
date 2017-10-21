5f7cc6ee2588f1fad710aa005f311567ac3a82a1
Eddie Penta <ecp4224@gmail.com>
2017-10-21 18:44:57 -0400

    Ported bullet animation

---
+++ Assets/Scripts/UI/Bullet.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class Bullet : MonoBehaviour
+{
+	private Vector3 origin, originScale;
+	public Vector3 speed = new Vector3(0f, 0f, 2.5f);
+	
+	// Use this for initialization
+	void Start ()
+	{
+		origin = transform.position;
+		originScale = transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(speed);
+	}
+
+	public void DoTheThing(GameObject obj)
+	{
+		
+		transform.position = obj.transform.position;
+
+		if (obj.name == "Exit" || obj.name == "Options")
+		{
+			transform.localScale = originScale / 2f;
+		}
+		else
+		{
+			transform.localScale = originScale;
+		}
+	}
+
+	public void UndoTheThing()
+	{
+		transform.position = origin;
+	}
+}
